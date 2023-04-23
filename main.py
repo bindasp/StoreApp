499bd444fda529f7d918604eb229e3d8138b9471
unknown <bindas.patryk@gmail.com>
2023-04-23 11:33:16 +0200

    theme

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton,
 class DemoApp(MDApp):
 
     def build(self):
+        self.theme_cls.primary_palette="Green"
+        self.theme_cls.primary_hue="A700"
+        self.theme_cls.theme_style="Dark"
         screen = Screen()
         btn_flat = MDRectangleFlatButton(text='Hello world', pos_hint= {'center_x': 0.5, 'center_y': 0.5})
 
         icon_btn = MDFloatingActionButton(icon='facebook', pos_hint= {'center_x': 0.5, 'center_y': 0.5})
-        screen.add_widget(icon_btn)
+        screen.add_widget(btn_flat)
 
         return screen
 DemoApp().run()
