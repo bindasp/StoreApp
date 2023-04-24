98e1214368613083add5c4c8aadbae057614ae78
unknown <bindas.patryk@gmail.com>
2023-04-24 17:16:15 +0200

    datatable

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
-from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDFloatingActionButton
-from kivymd.uix.textfield import MDTextField
-from kivy.lang import Builder
-from helpers import username_helper
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.list import OneLineListItem, MDList, ThreeLineIconListItem, IconLeftWidget, ThreeLineAvatarListItem, ImageLeftWidget
-from kivy.uix.scrollview import ScrollView
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
 
-list_helper = """
-Screen:
-    ScrollView:
-        MDList:
-            id: container
-                
-
-
-
-"""
 
 class DemoApp(MDApp):
 
     def build(self):
 
-        screen = Builder.load_string(list_helper)
+        screen = Screen()
+        table = MDDataTable(pos_hint= {'center_x': 0.5, "center_y": 0.5}, 
+            size_hint =(0.9, 0.6 ),
+            check=True,
+            rows_num=10,
+            column_data=[
+            ("No.", dp(18)),
+            ("Food", dp(20)),
+            ("Calories", dp(20))
+        ],
+        row_data=[
+            ("1","Burger", "300"),
+            ("2","Oats", "150"),
+            ("3","Oats", "150"),
+            ("4","Oats", "150"),
+            ("5","Oats", "150"),
+            ("6","Oats", "150"),
+            ("7","Oats", "150"),
+            ("8","Oats", "150"),
+            ("9","Oats", "150"),
+            
 
+        ]
+        )
+        table.bind(on_check_press=self.check_press)
+        table.bind(on_row_press=self.row_press)
+        
+        screen.add_widget(table)
+    
         return screen
     
-    def on_start(self):
-        for i in range(20):
-            items = OneLineListItem(text = 'Item ' +  str(i))
-            self.root.ids.container.add_widget(items)
+    def check_press(self, instance_table, current_row):
+        print(instance_table, current_row)
 
+    def row_press(self, instance_table, instance_row):
+        print(instance_table, instance_row)
+
+    
 DemoApp().run()
