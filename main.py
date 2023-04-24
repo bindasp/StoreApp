59f2e5f330bbd9eed9a9d8b2720d767c2062e7ec
unknown <bindas.patryk@gmail.com>
2023-04-24 19:51:49 +0200

    toolbar

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
 from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
-from kivymd.uix.datatables import MDDataTable
-from kivy.metrics import dp
+from kivy.lang import Builder
+from kivy.core.window import Window
 
+Window.size = (300,500)
+
+screen_helper = '''
+Screen:
+    BoxLayout:
+        orientation: "vertical"
+        MDTopAppBar:
+            title: "Demo"
+            left_action_items: [["menu", lambda x: app.navigation_draw()]]
+            right_action_items: [["clock", lambda x: app.navigation_draw()]]
+            elevation: 3
+        MDLabel:
+            text: "Hello World"
+            halign: "center"
+        MDBottomAppBar:
+            MDTopAppBar:
+                left_action_items: [["coffee", lambda x: app.navigation_draw()]]
+                mode:'end'
+                type: 'bottom'
+                icon: "language-python"
+                elevation: 0
+                on_action_button: app.navigation_draw()
+'''
 
 class DemoApp(MDApp):
 
     def build(self):
+        self.theme_cls.primary_palette = 'Red'
 
-        screen = Screen()
-        table = MDDataTable(pos_hint= {'center_x': 0.5, "center_y": 0.5}, 
-            size_hint =(0.9, 0.6 ),
-            check=True,
-            rows_num=10,
-            column_data=[
-            ("No.", dp(18)),
-            ("Food", dp(20)),
-            ("Calories", dp(20))
-        ],
-        row_data=[
-            ("1","Burger", "300"),
-            ("2","Oats", "150"),
-            ("3","Oats", "150"),
-            ("4","Oats", "150"),
-            ("5","Oats", "150"),
-            ("6","Oats", "150"),
-            ("7","Oats", "150"),
-            ("8","Oats", "150"),
-            ("9","Oats", "150"),
-            
+        return Builder.load_string(screen_helper)
 
-        ]
-        )
-        table.bind(on_check_press=self.check_press)
-        table.bind(on_row_press=self.row_press)
-        
-        screen.add_widget(table)
-    
-        return screen
-    
-    def check_press(self, instance_table, current_row):
-        print(instance_table, current_row)
+    def navigation_draw(self):
+        print("Navigation")
 
-    def row_press(self, instance_table, instance_row):
-        print(instance_table, instance_row)
-
-    
 DemoApp().run()
