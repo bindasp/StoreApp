edd629a14ee5a2c392003864e31c3a37e9e49581
unknown <bindas.patryk@gmail.com>
2023-04-25 12:04:01 +0200

    added login form

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
-from helpers import navigation_helper, screen_helper
+from helpers import screen_helper
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.properties import ObjectProperty
+from kivymd.uix.label import MDLabel
 
 Window.size = (300,500)
 
 class MenuScreen(Screen):
     pass
 
-class ProfileScreen(Screen):
-    pass
+class LoginScreen(Screen):
+    data = ObjectProperty(None)
+    text_field = ObjectProperty(None)
+
+    def login(self):
+        username = self.data.text 
+        password = self.text_field.text
+
+        print(username)
+        print(password)
 
 class UploadScreen(Screen):
     pass
@@ -18,19 +28,17 @@ class UploadScreen(Screen):
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
 
-sm.add_widget(ProfileScreen(name='profile'))
+sm.add_widget(LoginScreen(name='profile'))
 
 sm.add_widget(UploadScreen(name='upload'))
-
 
 class DemoApp(MDApp):
 
     def build(self):
-        self.theme_cls.primary_palette = 'Red'
-
+        self.theme_cls.primary_palette="Green"
+        self.theme_cls.primary_hue="A700"
+        self.theme_cls.theme_style="Dark"
         return Builder.load_string(screen_helper)
 
-    def navigation_draw(self):
-        print("Navigation")
 
 DemoApp().run()
